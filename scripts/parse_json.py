a56472597c06b303a1a71ba7fa945be86fb76f25
Shashwot Musyaju <shashwot.musyaju@cotiviti.com>
2024-04-23 00:04:56 +0545

    altered merge operation to make it more effecient

--- scripts/parse_json.py
+++ scripts/parse_json.py
@@ -55,8 +55,8 @@ try:
 
     in_network_data = []
     for in_network in json_data['in_network']:
-        for y in range (0,len(json_data['in_network'])-1):
-        #for y in range (0,50):
+        #for y in range (0,len(json_data['in_network'])-1):
+        for y in range (0,5):
             for negotiated_rates in json_data['in_network'][y]['negotiated_rates']:
                 for neg_data in negotiated_rates['negotiated_prices']:
                     in_network_dict = {
@@ -69,10 +69,11 @@ try:
                         'provider_references': negotiated_rates['provider_references'][0],
                         'negotiated_prices': neg_data['negotiated_rate'],
                         'service_code': neg_data['service_code'][0], 
+                        #'service_code': [a if len(neg_data['service_code'])>1 else neg_data['service_code'] for a in neg_data['service_code']], 
                         'negotiated_type': neg_data['negotiated_type'],
                         'expiration_date': neg_data['expiration_date'],
                         'billing_class': neg_data['billing_class'],
-                        'billing_code_modifier': neg_data['billing_code_modifier'],
+                        #'billing_code_modifier': neg_data['billing_code_modifier'],
                         'additional_information': neg_data['additional_information'].strip()
                     }
                     in_network_data.append(in_network_dict)
@@ -82,12 +83,23 @@ try:
     # Creating DataFrame to store provider data   
     df2 = pd.DataFrame(in_network_data)
 
+    #print(df2.loc[(df2['billing_code'] == '20552') & (df2['provider_references'] == 5)])
+
+    df = pd.merge(df1[['reporting_entity_name','npi','provider_group_id']]
+                  ,df2[['name','billing_code_type','billing_code','negotiation_arrangement','negotiated_prices','negotiated_type','expiration_date','service_code','provider_references']]
+                  ,how='inner'
+                  ,left_on = 'provider_group_id'
+                  , right_on= 'provider_references'
+                  )
+    df = df.drop(columns='provider_references').drop_duplicates()
 
     # Output Files
     print('Task Started : Writing to CSV files\n')
 
-    df1.to_csv(outputdirectory + '\\providerreferences.csv')
-    df2.to_csv(outputdirectory + '\\innetworkrates.csv')
+    #df1.to_csv(outputdirectory + '\\providerreferences.csv')
+    #df2.to_csv(outputdirectory + '\\innetworkrates.csv')
+
+    df.to_csv(outputdirectory + '\\combineddata.csv')
 
     print('Task Completed : Writing to CSV files\n')
 
